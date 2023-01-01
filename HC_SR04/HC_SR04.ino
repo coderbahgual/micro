def0f3348a3f9d074824d18d2c9612407693dfae
oesley <oesleyrm@gmail.com>
2023-01-01 17:17:16 -0300

    Ajustado HC

--- HC_SR04/HC_SR04.ino
+++ HC_SR04/HC_SR04.ino
@@ -5,13 +5,17 @@
 //Carrega a biblioteca do sensor ultrassonico
 #include <Ultrasonic.h>
 //Define os pinos para o trigger e echo
-#define pino_trigger 4
-#define pino_echo 5
+#define pino_trigger 2
+#define pino_echo 3
 //Inicializa o sensor nos pinos definidos acima
 Ultrasonic ultrasonic(pino_trigger, pino_echo);
+
+const int buzzerPin = 8;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Lendo dados do sensor...");
+  pinMode(buzzerPin, OUTPUT);
 }
 void loop() {
   //Le as informacoes do sensor, em cm e pol
@@ -22,6 +26,9 @@ void loop() {
   //Exibe informacoes no serial monitor
   Serial.print("Distancia em cm: ");
   Serial.print(cmMsec);
+  if (cmMsec <= 10.0) {
+    tone(buzzerPin, 440, 1000);
+  }
   Serial.print(" - Distancia em polegadas: ");
   Serial.println(inMsec);
   delay(1000);
