362b2f44c00417a30843a095fbc8653ce6615ca6
oesley <oesleyrm@gmail.com>
2023-01-06 14:30:25 -0300

    funcoes

--- CARRO_V1/CARRO_V1.ino
+++ CARRO_V1/CARRO_V1.ino
@@ -1,19 +1,26 @@
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+//Define os pinos para o trigger e echo
+#define pinoTrigger 2
+#define pinoEcho 3
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pinoTrigger, pinoEcho);
+
+const int buzzer = 12; // Define o Pino 13 como pino do Buzzer.
+
 // Define os pinos de utilização do Driver L298.
-const int motorLF  = 11;    // Pin  5 of L293.
-const int motorLB  = 10;    // Pin  6 of L293.
-const int motorDB  = 9;   // Pin 10 of L293.
-const int motorDF  = 8;   // Pin 9 of L293.
+const int motorLF = 11; // motor esquerdo gira pra frente
+const int motorLB = 10; // motor esquerdo gira pra tras
+const int motorDB = 9;  // motor direito gira pra frente
+const int motorDF = 8;  // motor direito gira pra tras
 
-const int buzzer = 12;   // Define o Pino 13 como pino do Buzzer.
+#define MAX_SPEED 200
+#define MAX_SPEED_OFFSET 20
 
-// Variáveis Úteis
-int i = 0;
-int j = 0;
-int state_rec;
-int vSpeed = 200;   // Define velocidade padrão 0 < x < 255.
-char state;
+float distancia = 0;
+int speedSet = 0;
 
-
+#define turn_amount 500
 
 void setup() {
   // Inicializa as portas como entrada e saída.
@@ -22,7 +29,6 @@ void setup() {
   pinMode(motorDB, OUTPUT);
   pinMode(motorDF, OUTPUT);
   pinMode(buzzer, OUTPUT);
-//  pinMode(BTState, INPUT);
 
   // Inicializa a comunicação serial em 9600 bits.
   Serial.begin(9600);
@@ -30,119 +36,87 @@ void setup() {
 }
 
 void loop() {
-  // Para o carro quando a conexão com Bluetooth é perdida ou desconectada.
-//  if (digitalRead(BTState) == LOW) {
-    state = 'F';
-//  }
+  float distanciaAux;
+  long microsec = ultrasonic.timing();
+  distancia = ultrasonic.convert(microsec, Ultrasonic::CM); // info sensor em centimetros
+  delay(40);
+  Serial.print("Distancia atual: ");
+  Serial.println(distancia);
+  moveForward();
+  if (distancia <= 0.0) {
+    // tone(buzzer, 440, 1000);
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+    digitalWrite(buzzer, HIGH);
+    
+    Serial.println("Object Detected");
+    moveStop();
+    delay(100);
+    moveBackward();
+    delay(300);
+    moveStop();
+    delay(200);
+    distanciaAux = ultrasonic.convert(microsec, Ultrasonic::CM); // info sensor em centimetros
+    Serial.print("Distancia aux: ");
+    Serial.println(distanciaAux);
+    delay(200);
 
-  // Salva os valores da variável 'state'
-//  if (Serial.available() > 0) {
-//    state_rec = Serial.read();
-//    state = state_rec;//.trim();
-//    Serial.println(vSpeed);
-//  }
-
-  // Altera a velocidade de acordo com valores especificados.
-  if (state == '0') {
-    vSpeed = 0;
-  }
-  else if (state == '4') {
-    vSpeed = 100;
-  }
-  else if (state == '6') {
-    vSpeed = 155;
-  }
-  else if (state == '7') {
-    vSpeed = 180;
-  }
-  else if (state == '8') {
-    vSpeed = 200;
-  }
-  else if (state == '9') {
-    vSpeed = 230;
-  }
-  else if (state == 'q') {
-    vSpeed = 255;
-  }
-
-  if (state != 'S') {
-    Serial.print(state);
-  }
-
-  // Se o estado recebido for igual a 'F', o carro se movimenta para frente.
-  if (state == 'F') {
-    analogWrite(motorDB, 0);
-    analogWrite(motorLF, vSpeed);
-    analogWrite(motorLB, 0);
-    analogWrite(motorDF, vSpeed);
-  }
-
-    else if (state == 'I') {  // Se o estado recebido for igual a 'I', o carro se movimenta para Frente Esquerda.
-    analogWrite(motorLF, vSpeed); 
-    analogWrite(motorLB, 0);
-    analogWrite(motorDB, 100);    
-    analogWrite(motorDF, 0);
-  }
-
-    else if (state == 'G') {   // Se o estado recebido for igual a 'G', o carro se movimenta para Frente Direita.
-    analogWrite(motorLF, 100); 
-    analogWrite(motorLB, 0);
-    analogWrite(motorDB, vSpeed);      
-    analogWrite(motorDF, 0);
-  }
-
-  else if (state == 'B') { // Se o estado recebido for igual a 'B', o carro se movimenta para trás.
-    analogWrite(motorLF, 0);
-    analogWrite(motorDB, 0);
-    analogWrite(motorDF, vSpeed);
-    analogWrite(motorLB, vSpeed);
-  }
-
-   else if (state == 'H') {  // Se o estado recebido for igual a 'H', o carro se movimenta para Trás Esquerda.
-    analogWrite(motorLF, 0);   
-    analogWrite(motorLB, vSpeed);
-    analogWrite(motorDB, 0); 
-    analogWrite(motorDF, 100);
-  }
-  
-  else if (state == 'J') {  // Se o estado recebido for igual a 'J', o carro se movimenta para Trás Direita.
-    analogWrite(motorLF, 0);   
-    analogWrite(motorLB, 100);
-    analogWrite(motorDB, 0);   
-    analogWrite(motorDF, vSpeed);
-  }
-
-  else if (state == 'L') {   // Se o estado recebido for igual a 'L', o carro se movimenta para esquerda.
-    analogWrite(motorLF, 0);
-    analogWrite(motorLB, vSpeed);
-    analogWrite(motorDB, vSpeed);
-    analogWrite(motorDF, 0);
-  }
-  else if (state == 'R') {   // Se o estado recebido for igual a 'R', o carro se movimenta para direita.
-    analogWrite(motorLF, vSpeed);
-    analogWrite(motorLB, 0);
-    analogWrite(motorDB, 0);
-    analogWrite(motorDF, vSpeed);
-  }
-  else if (state == 'S') {   // Se o estado recebido for igual a 'S', o carro permanece parado.
-    analogWrite(motorLF, 0);
-    analogWrite(motorLB, 0);
-    analogWrite(motorDB, 0);
-    analogWrite(motorDF, 0);
-  }
-
-
-  else if (state == 'V') { // Se o estado recebido for iguala 'V', aciona a buzina.
-    if (j == 0) {
-      tone(buzzer, 1000);
-      j = 1;
+    if(distanciaAux >= distancia) {
+      turnRight();
+      moveStop();
+    } else {
+      turnLeft();
+      turnLeft();
+      moveStop();
     }
-    else if (j == 1) {
-      noTone(buzzer);
-      j = 0;
-    }
-    state = 'n';
+  } else {
+    moveForward();
   }
-
-
 }
+
+void motores(int vMLF, int vMDF, int vMLB, int vMDB) {
+  analogWrite(motorLF, vMLF);
+  analogWrite(motorLB, vMLB);
+  analogWrite(motorDF, vMDF);
+  analogWrite(motorDB, vMDB);
+}
+
+void moveStop() {
+  motores(0, 0, 0, 0);
+}
+  
+void moveForward() {
+  // motores(MAX_SPEED, 0, MAX_SPEED, 0);
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) 
+  // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motores(speedSet, 0, speedSet, 0);
+    // motores(speedSet, 0, speedSet+MAX_SPEED_OFFSET, 0);
+    delay(5);
+  }
+}
+
+void moveBackward() {
+  // motores(0, MAX_SPEED, 0, MAX_SPEED);
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) 
+  // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motores(0, speedSet, 0, speedSet);
+    // motores(0, speedSet, 0, speedSet+MAX_SPEED_OFFSET);
+    delay(5);
+  }
+}  
+
+void turnRight() {
+  Serial.println("Turning Right");
+  motores(MAX_SPEED, 0, 0, MAX_SPEED);
+  delay(turn_amount);
+  motores(MAX_SPEED, 0, MAX_SPEED, 0);
+} 
+ 
+void turnLeft() {
+  Serial.println("Turning Left");
+  motores(0, MAX_SPEED, MAX_SPEED, 0);
+  delay(turn_amount);
+  motores(MAX_SPEED, 0, MAX_SPEED, 0);
+}  
